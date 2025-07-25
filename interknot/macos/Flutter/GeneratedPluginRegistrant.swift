6b51b0ae5373280e639d96912e3da984b17ecb5a
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-07-25 14:02:19 +0700

    Re-Init app

--- interknot/macos/Flutter/GeneratedPluginRegistrant.swift
+++ interknot/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,26 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import audio_service
-import audio_session
-import file_picker
-import flutter_local_notifications
-import just_audio
-import path_provider_foundation
-import shared_preferences_foundation
-import sqflite_darwin
-import url_launcher_macos
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
-  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
