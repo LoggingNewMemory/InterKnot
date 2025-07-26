fc921f6d5931a63e14a3a11a44db1d8a546e1a49
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-07-26 22:59:04 +0700

    Checkpoint: Next- Add header

--- interknot/lib/main.dart
+++ interknot/lib/main.dart
@@ -8,11 +8,21 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
+  StatelessElement createElement() {
+    // TODO: implement createElement
+    return super.createElement();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black, // This to change BG Color
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Welcome to Inter-Knot',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
         ),
       ),
     );
